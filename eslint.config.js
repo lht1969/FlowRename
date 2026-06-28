0d322a8640c1cff083e1cf592f8bab6875df9b6b
lht1969 <2265147096@qq.com>
2026-06-28 21:14:02 +0800

    chore: add ESLint flat config for Svelte + TypeScript

---
+++ eslint.config.js
@@ -0,0 +1,98 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import svelte from "eslint-plugin-svelte";
+import prettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...svelte.configs["flat/recommended"],
+  prettier,
+  ...svelte.configs["flat/prettier"],
+  {
+    ignores: [
+      "dist/",
+      "src-tauri/",
+      "target/",
+      ".svelte-kit/",
+      "node_modules/",
+    ],
+  },
+  {
+    languageOptions: {
+      globals: {
+        // Browser globals
+        window: "readonly",
+        document: "readonly",
+        console: "readonly",
+        navigator: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+        fetch: "readonly",
+        URL: "readonly",
+        URLSearchParams: "readonly",
+        File: "readonly",
+        FileReader: "readonly",
+        Blob: "readonly",
+        FormData: "readonly",
+        Event: "readonly",
+        CustomEvent: "readonly",
+        MouseEvent: "readonly",
+        KeyboardEvent: "readonly",
+        DragEvent: "readonly",
+        ClipboardEvent: "readonly",
+        InputEvent: "readonly",
+        HTMLElement: "readonly",
+        HTMLInputElement: "readonly",
+        HTMLSelectElement: "readonly",
+        HTMLTextAreaElement: "readonly",
+        HTMLDivElement: "readonly",
+        HTMLButtonElement: "readonly",
+        HTMLSpanElement: "readonly",
+        HTMLHeadingElement: "readonly",
+        HTMLUListElement: "readonly",
+        HTMLLIElement: "readonly",
+        HTMLDialogElement: "readonly",
+        HTMLPreElement: "readonly",
+        HTMLElementTagNameMap: "readonly",
+        CSSStyleDeclaration: "readonly",
+        IntersectionObserver: "readonly",
+        MutationObserver: "readonly",
+        requestAnimationFrame: "readonly",
+        cancelAnimationFrame: "readonly",
+        getComputedStyle: "readonly",
+        matchMedia: "readonly",
+        performance: "readonly",
+        crypto: "readonly",
+        TextDecoder: "readonly",
+        TextEncoder: "readonly",
+        // SvelteKit / Vite globals
+        __sveltekit_12kjl7c: "readonly",
+      },
+    },
+  },
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-require-imports": "off",
+      "no-console": "warn",
+      "svelte/require-each-key": "off",
+      "svelte/no-unused-svelte-ignore": "off",
+      "svelte/prefer-writable-derived": "off",
+    },
+  }
+);
